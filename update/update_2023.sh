7a01bd8ad383859dc6b8fd4fd2bf35c270ad0bb6
lls <lls.homeoffice@gmail.com>
2024-01-08 18:24:13 -0300

    Add files remotely

--- update/update_2023.sh
+++ update/update_2023.sh
@@ -419,54 +419,6 @@ OUTUBRO(2023){
 
 }
 
-(12/10/2023){
-	
-()Criar metodos para fazer todas as funcionalidades JS do Milho {
-	
-	() Arquivo: modulos/milho/sh/jquery-lls-milho.sh
-	
-	Conferir arquivos existentes nos diretorios com os arquivos informados nos arquivos para excluir abaixo:
-	
-	Excluir o arquivo:
-	() Arquivo: modulos/milho/sh/compila_milho_js.sh
-	() Arquivo: modulos/milho/sh/jquery-lls-umidade.sh
-	() Arquivo: modulos/milho/sh/jquery-lls-componentes-milho.sh
-	() Arquivo: modulos/milho/sh/jquery-lls-baixamilho.sh
-	() Arquivo: modulos/milho/sh/jquery-lls-entmilho.sh
-	() Arquivo: modulos/milho/sh/jquery-lls-faturamilho.sh
-	() Arquivo: modulos/milho/sh/jquery-lls-laudomilho.sh
-	() Arquivo: modulos/milho/sh/jquery-lls-movimentomilho.sh
-	() Arquivo: modulos/milho/sh/jquery-lls-resumomilho.sh
-	() Arquivo: modulos/milho/sh/jquery-lls-saimilho.sh
-	() Arquivo: modulos/milho/sh/jquery-lls-servicomilho.sh
-	() Arquivo: modulos/milho/sh/jquery-lls-sintetizamilho.sh
-	() Arquivo: modulos/milho/sh/jquery-lls-tramilho.sh
-	() Arquivo: modulos/milho/sh/jquery-lls-modulos.txt
-	
-}
-
-() Excluir Arquivos {
-	() Arquivo: modulos/milho/sh/jquery-lls-componentes-milho.js
-}
-
-() Juntar Arquivos JS dos Modulos {
-	
-	Milho, Café e Balança
-	
-	Criar um unico arquivo: jquery-lls-modulos.js
-	
-}
-
-() Verificar exclusão {
-	() Arquivo: js/componentes/cadastro/getJson.js
-	() Arquivo: js/componentes/login/carregaCss.js
-	() Arquivo: js/componentes/login/carregaCssJs.js
-	() Arquivo: sh/jquery-lls-cadastro.sh
-	() Arquivo: sh/jquery-lls-core.sh
-}
-
-}
-
 }
 
 }
