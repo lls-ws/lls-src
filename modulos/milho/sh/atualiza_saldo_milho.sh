96699f514a2ff6abfbffda16229ecbe63b891039
lls <lls.homeoffice@gmail.com>
2024-01-28 14:19:14 -0300

    Add files remotely

--- modulos/milho/sh/atualiza_saldo_milho.sh
+++ modulos/milho/sh/atualiza_saldo_milho.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-# Script para atualizar os saldos das fichas de milho
+# Script para Atualizar os Saldos das Fichas de Milho
 #
 # email: lls.homeoffice@gmail.com
+
+PATH=.:$(dirname $0):$PATH
+. lib/mysql.lib		|| exit 1
 
 zerando()
 {
 	
 	echo "Zerando Entradas|Saidas|Saldo da ficha de Milho..."
 	
-	mysql -u root --password=Uber#739200 -D bd_lls -e "\
+	${CMD_BASE} -e "\
 			UPDATE Milho milho \
 				SET milho.bruto = 0, \
 					milho.liquidoEntrada = 0, \
@@ -24,7 +27,7 @@ atualiza()
 	
 	echo "Atualizando Entradas|Saidas|Saldo da ficha de Milho..."
 	
-	mysql -u root --password=Uber#739200 -D bd_lls -e "\
+	${CMD_BASE} -e "\
 			UPDATE Milho milho \
 				
 				LEFT JOIN ( 
@@ -80,7 +83,7 @@ atualiza()
 lista()
 {
 
-	mysql -u root --password=Uber#739200 -D bd_lls -e "\
+	${CMD_BASE} -e "\
 			SELECT 	produtor.nome AS PRODUTOR, \
 					fazendaProdutor.nome AS FAZENDA, \
 					liquidoEntrada AS ENTRADAS, \
@@ -101,7 +104,7 @@ lista()
 total()
 {
 
-	mysql -u root --password=Uber#739200 -D bd_lls -e "\
+	${CMD_BASE} -e "\
 			SELECT 	SUM(liquidoEntrada) AS ENTRADAS, \
 					SUM(liquidoSaida) AS SAIDAS, \
 					SUM(total) AS SALDO \
@@ -110,12 +113,22 @@ total()
 
 }
 
-teste()
+check_id()
 {
 	
-	echo "Verificando Entradas|Saidas|Saldo:"
+	ID="${QUERY}"
 	
-	mysql -u root --password=Uber#739200 -D bd_lls -e "\
+	if [ -z "${ID}" ]; then
+	
+		echo "ID Not Set!"
+		echo "Use: sudo `basename $0` check [ID]"
+		exit 1
+		
+	fi
+	
+	echo "Verificando Entradas|Saidas|Saldo ID: ${ID}"
+	
+	${CMD_BASE} -e "\
 			SELECT	IFNULL(entradaAnterior.total,0) AS ENTRADAS_ANTERIOR, \
 					IFNULL(saidaAnterior.total,0) AS SAIDAS_ANTERIOR, \
 					IFNULL(entradaAtual.total,0) AS ENTRADAS_ATUAL, \
@@ -165,16 +178,65 @@ teste()
 					) AS saidaAtual \
 					ON milho.id = saidaAtual.fazenda_id \
 					
-					WHERE milho.id in (389, 968)"
+					WHERE milho.id in (${ID})"
 					
 }
 
-exclui()
+find_produtor_id()
 {
 	
-	echo "Excluindo registro errado..."
-	mysql -u root --password=Uber#739200 -D bd_lls -e "delete from Saimilho where id = 63"
-
+	NAME="${QUERY}"
+	
+	if [ -z "${NAME}" ]; then
+	
+		echo "NAME Not Set!"
+		echo "Use: sudo `basename $0` find_produtor [NAME]"
+		exit 1
+		
+	fi
+	
+	echo "Find name: ${NAME}"
+	
+	${CMD_BASE} -e "SELECT id, nome FROM Produtor WHERE nome LIKE '%${NAME}%'"
+					
 }
 
-atualiza
+find_fazenda_id()
+{
+	
+	ID="${QUERY}"
+	
+	if [ -z "${ID}" ]; then
+	
+		echo "ID Not Set!"
+		echo "Use: sudo `basename $0` find_fazenda [ID]"
+		exit 1
+		
+	fi
+	
+	echo "Find ID: ${ID}"
+	
+	${CMD_BASE} -e "SELECT id, nome FROM FazendaProdutor WHERE produtor_id IN (${ID})"
+					
+}
+
+QUERY="$2"
+
+case "$1" in
+	update)    	
+		atualiza
+		;; 
+	check)
+		check_id "${QUERY}"
+		;;
+	find_produtor)
+		find_produtor_id "${QUERY}"
+		;;
+	find_fazenda)
+		find_fazenda_id "${QUERY}"
+		;;
+	*)
+		echo "Use: $(basename $0) {update|check|find_produtor|find_fazenda}"
+		exit 1
+		;;
+esac
