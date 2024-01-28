21f12bdcba597a3d774ef4bf424a2ba20c597c58
lls <lls.homeoffice@gmail.com>
2024-01-28 18:16:52 -0300

    Add files remotely

--- sh/jquery-lls-cadastro.sh
+++ sh/jquery-lls-cadastro.sh
@@ -96,6 +96,67 @@ copia_arquivos_js_core()
 	
 }
 
+cria_arq_cadastro_tabela()
+{
+	
+	NOME="$1"
+	
+	NOME_MIN=$(echo "$NOME" | tr '[:upper:]' '[:lower:]')
+	
+	COMPONENTES+=(
+		"$NOME_MIN/pega$NOME.js"
+		"$NOME_MIN/pegaTabela$NOME.js"
+		"$NOME_MIN/setLinhaTabela$NOME.js"
+		"$NOME_MIN/mudaLinhaTabela$NOME.js"
+		"$NOME_MIN/remove$NOME.js"
+		"$NOME_MIN/altera$NOME.js"
+		"$NOME_MIN/removeTotalTabela$NOME.js"
+	)
+	
+	cria_arq_cadastro_formulario $NOME
+	
+}
+
+cria_arq_relatorio(6)
+{
+	
+	NOME="$1"
+	
+	NOME_MIN=$(echo "$NOME" | tr '[:upper:]' '[:lower:]')
+	
+	COMPONENTES+=(
+		"$NOME_MIN/pegaNomeColunas$NOME.js"
+		"$NOME_MIN/formataDados$NOME.js"
+		"$NOME_MIN/setDadosTabela$NOME.js"
+		"$NOME_MIN/setDadosRodape$NOME.js"
+		"$NOME_MIN/pegaProcura$NOME.js"
+		"$NOME_MIN/formularioRelatorio$NOME.js"
+	)
+	
+	cria_arq $ARQ $COMPONENTES
+	
+}
+
+
+cria_arq_relatorio_cadastro(6)
+{
+	
+	NOME="$1"
+	
+	NOME_MIN=$(echo "$NOME" | tr '[:upper:]' '[:lower:]')
+	
+	COMPONENTES+=(
+		"$NOME_MIN/eventoSalvar$NOME.js"
+		"$NOME_MIN/formulario$NOME.js"
+		"$NOME_MIN/validarFormulario$NOME.js"
+		"$NOME_MIN/pegaDadosFormulario$NOME.js"
+		"$NOME_MIN/limpaDadosFormulario$NOME.js"
+		"$NOME_MIN/pegaNomeColunas$NOME.js"
+	)
+	
+	cria_arq $ARQ $COMPONENTES
+	
+}
 
 
 cria_arq_cadastro(7)
@@ -135,67 +196,6 @@ cria_arq_cadastro_formulario(6)
 		"$NOME_MIN/pegaNomeColunas$NOME.js"
 	)
 
-	cria_arq $ARQ $COMPONENTES
-	
-}
-
-cria_arq_cadastro_tabela()
-{
-	
-	NOME="$1"
-	
-	NOME_MIN=$(echo "$NOME" | tr '[:upper:]' '[:lower:]')
-	
-	COMPONENTES+=(
-		"$NOME_MIN/pega$NOME.js"
-		"$NOME_MIN/pegaTabela$NOME.js"
-		"$NOME_MIN/setLinhaTabela$NOME.js"
-		"$NOME_MIN/mudaLinhaTabela$NOME.js"
-		"$NOME_MIN/remove$NOME.js"
-		"$NOME_MIN/altera$NOME.js"
-		"$NOME_MIN/removeTotalTabela$NOME.js"
-	)
-	
-	cria_arq_cadastro_formulario $NOME
-	
-}
-
-cria_arq_relatorio()
-{
-	
-	NOME="$1"
-	
-	NOME_MIN=$(echo "$NOME" | tr '[:upper:]' '[:lower:]')
-	
-	COMPONENTES+=(
-		"$NOME_MIN/pegaNomeColunas$NOME.js"
-		"$NOME_MIN/formataDados$NOME.js"
-		"$NOME_MIN/setDadosTabela$NOME.js"
-		"$NOME_MIN/setDadosRodape$NOME.js"
-		"$NOME_MIN/pegaProcura$NOME.js"
-		"$NOME_MIN/formularioRelatorio$NOME.js"
-	)
-	
-	cria_arq $ARQ $COMPONENTES
-	
-}
-
-cria_arq_relatorio_cadastro()
-{
-	
-	NOME="$1"
-	
-	NOME_MIN=$(echo "$NOME" | tr '[:upper:]' '[:lower:]')
-	
-	COMPONENTES+=(
-		"$NOME_MIN/eventoSalvar$NOME.js"
-		"$NOME_MIN/formulario$NOME.js"
-		"$NOME_MIN/validarFormulario$NOME.js"
-		"$NOME_MIN/pegaDadosFormulario$NOME.js"
-		"$NOME_MIN/limpaDadosFormulario$NOME.js"
-		"$NOME_MIN/pegaNomeColunas$NOME.js"
-	)
-	
 	cria_arq $ARQ $COMPONENTES
 	
 }
--- update/update_2023.sh
+++ update/update_2023.sh
@@ -423,31 +423,40 @@ OUTUBRO(2023){
 
 (Semana 3) {
 
-(26/01/2024){
+(27/01/2024){
 	
-()Criar metodos para fazer todas as funcionalidades JS do Milho {
+(OK)Criar metodos para fazer todas as funcionalidades JS do Milho {
 	
 	(ok) Arquivo: modulos/milho/sh/jquery-lls-milho.sh
 	
-	Conferir arquivos existentes nos diretorios com os arquivos informados nos arquivos para excluir abaixo:
+}
+
+}
+
+(28/01/2024){
+
+(OK) Conferir arquivos existentes nos diretorios com os arquivos informados nos arquivos para excluir abaixo {
 	
-	Excluir o arquivo:
-	() Arquivo: modulos/milho/sh/compila_milho_js.sh
-	() Arquivo: modulos/milho/sh/jquery-lls-umidade.sh
-	() Arquivo: modulos/milho/sh/jquery-lls-componentes-milho.sh
-	() Arquivo: modulos/milho/sh/jquery-lls-baixamilho.sh
-	() Arquivo: modulos/milho/sh/jquery-lls-entmilho.sh
-	() Arquivo: modulos/milho/sh/jquery-lls-faturamilho.sh
-	() Arquivo: modulos/milho/sh/jquery-lls-laudomilho.sh
-	() Arquivo: modulos/milho/sh/jquery-lls-movimentomilho.sh
-	() Arquivo: modulos/milho/sh/jquery-lls-resumomilho.sh
-	() Arquivo: modulos/milho/sh/jquery-lls-saimilho.sh
-	() Arquivo: modulos/milho/sh/jquery-lls-servicomilho.sh
-	() Arquivo: modulos/milho/sh/jquery-lls-sintetizamilho.sh
-	() Arquivo: modulos/milho/sh/jquery-lls-tramilho.sh
-	() Arquivo: modulos/milho/sh/jquery-lls-modulos.txt
+	(ok) Arquivo: modulos/milho/sh/jquery-lls-modulos.txt
+	(ok) Arquivo: modulos/milho/sh/compila_milho_js.sh
+	(ok) Arquivo: modulos/milho/sh/jquery-lls-umidade.sh
+	(ok) Arquivo: modulos/milho/sh/jquery-lls-baixamilho.sh
+	(ok) Arquivo: modulos/milho/sh/jquery-lls-entmilho.sh
+	(ok) Arquivo: modulos/milho/sh/jquery-lls-saimilho.sh
+	(ok) Arquivo: modulos/milho/sh/jquery-lls-laudomilho.sh
+	(ok) Arquivo: modulos/milho/sh/jquery-lls-milho.sh
+	(ok) Arquivo: modulos/milho/sh/jquery-lls-tramilho.sh
+	(ok) Arquivo: modulos/milho/sh/jquery-lls-componentes-milho.sh
+	(ok) Arquivo: modulos/milho/sh/jquery-lls-faturamilho.sh
+	(ok) Arquivo: modulos/milho/sh/jquery-lls-movimentomilho.sh
+	(ok) Arquivo: modulos/milho/sh/jquery-lls-servicomilho.sh
+	(ok) Arquivo: modulos/milho/sh/jquery-lls-sintetizamilho.sh
 	
 }
+
+}
+
+(29/01/2024){
 
 () Verificar exclusão {
 	() Arquivo: js/componentes/cadastro/getJson.js
