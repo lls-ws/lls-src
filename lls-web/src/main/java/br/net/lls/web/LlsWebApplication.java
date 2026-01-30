b7426e66470c0528818ed0c9b60b5a5221ba15e0
lls <lls.homeoffice@gmail.com>
2026-01-30 19:32:29 -0300

    Add files remotely

---
+++ lls-web/src/main/java/br/net/lls/web/LlsWebApplication.java
@@ -0,0 +1,13 @@
+package br.net.lls.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlsWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlsWebApplication.class, args);
+	}
+
+}
